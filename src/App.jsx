f3e3d80e09a0048fbd63edce1df7dd0a4dcec3be
rackaaf <rackaaf@gmail.com>
2025-07-09 18:59:28 +0700

    firsttt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 function App() {
-
   return (
     <>
-      <h1 className="text-4xl text-pink-500 text-center p-5">Haii BROWWW</h1>
+      {/* <h1 className="text-4xl text-pink-500 text-center p-5">Haii BROWWW</h1> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo"></div>
+      <ul className="menu">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Project</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
